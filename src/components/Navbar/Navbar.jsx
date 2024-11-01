df7772e391d655167a54faf1ebe2fb94e4b49f03
DairanDoo <your_email@example.com>
2024-11-01 11:01:46 -0400

    Navbar casi terminado, todavia me falta mejorar la organización pero se ve bastante bien.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,51 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import logo_img from '/voz-estudiantil-proyecto/Voz-Estudiantil-Frontend/src/assets/images/VozEstudiantil_logo.png'; // Cambié a alias
+import { Navbar, Container } from 'react-bootstrap';
+import styles from './Navbar.module.css';
+import logo_img from '/voz-estudiantil-proyecto/Voz-Estudiantil-Frontend/src/assets/images/VozEstudiantil_logo.png';
 
 const TestingNavbar = () => {
   return (
-    <nav className="navbar navbar-light bg-light d-flex justify-content-between p-3">
-      <div className="d-flex align-items-center">
-        <img src={logo_img} alt="Logo" style={{ width: '50px' }} className="mr-2" />
-        <h1 className="h4 m-0">VozEstudiantil</h1>
-      </div>
-      <div className="input-group" style={{ width: '400px', border: '1px solid #343a40', borderRadius: '4px' }}>
-        <div className="input-group-prepend">
-          <span className="input-group-text bg-light border-0" style={{ borderRight: '1px solid #343a40' }}>
-            <i className="bi bi-search"></i>
-          </span>
-        </div>
-        <input type="text" className="form-control border-0" placeholder="Search" />
-      </div>
-      <div>
-        <button className="btn btn-info mx-2">Log In</button>
-        <button className="btn btn-info">Sign Up</button>
-      </div>
-    </nav>
+    <Navbar bg="light" expand="lg" className="p-3">
+      <Container fluid>
+        <Navbar.Brand className={styles.navbarBrand}>
+          <img src={logo_img} alt="Logo" className={styles.logo} />
+          <span className="h2 m-0">VozEstudiantil</span>
+        </Navbar.Brand>
+        
+        <Navbar.Toggle aria-controls="navbarResponsive" />
+
+        <Navbar.Collapse id="navbarResponsive" className="justify-content-between">
+          <div className={`${styles.searchBar} input-group d-none d-lg-flex mx-auto`}>
+            <div className="input-group-prepend">
+              <span className={`${styles.searchIcon} input-group-text border-0`}>
+                <i className="bi bi-search"></i>
+              </span>
+            </div>
+            <input type="text" className="form-control border-0" placeholder="Search" />
+          </div>
+          
+          <div className="d-none d-lg-flex align-items-center">
+            <button className={`btn btn-info mx-2 ${styles.buttonGroup}`}>Log In</button>
+            <button className={`btn btn-info ${styles.buttonGroup}`}>Sign Up</button>
+          </div>
+          
+          <div className={`${styles.mobileContainer} d-flex d-lg-none`}>
+            <div className={`${styles.searchBar} input-group mb-2`}>
+              <div className="input-group-prepend">
+                <span className={`${styles.searchIcon} input-group-text border-0`}>
+                  <i className="bi bi-search"></i>
+                </span>
+              </div>
+              <input type="text" className="form-control border-0" placeholder="Search" />
+            </div>
+            <div className="d-flex justify-content-around w-100">
+              <button className={`btn btn-info ${styles.buttonGroup}`}>Log In</button>
+              <button className={`btn btn-info ${styles.buttonGroup}`}>Sign Up</button>
+            </div>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
