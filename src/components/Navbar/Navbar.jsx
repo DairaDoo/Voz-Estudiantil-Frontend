034b6096e836685a5a1c1d8d79832f1b9a17da57
DairanDoo <your_email@example.com>
2024-11-03 19:05:22 -0400

    Dropdown incompleto, falta organizarlo pero ya el search bar se ve en moviles y en desktop.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,37 +7,35 @@ const TestingNavbar = () => {
   return (
     <Navbar bg="light" expand="lg" className="p-3">
       <Container fluid>
+        {/* Logo y Nombre */}
         <Navbar.Brand className={styles.navbarBrand}>
           <img src={logo_img} alt="Logo" className={styles.logo} />
-          <span className="h2 m-0">VozEstudiantil</span>
+          <span className="h2 m-0 d-none d-lg-inline">VozEstudiantil</span>
         </Navbar.Brand>
-        
+
+        {/* Barra de búsqueda siempre visible */}
+        <div className={`${styles.searchBar} input-group mx-auto`}>
+          <div className="input-group-prepend">
+            <span className={`${styles.searchIcon} input-group-text border-0`}>
+              <i className="bi bi-search"></i>
+            </span>
+          </div>
+          <input type="text" className="form-control border-0" placeholder="Search" />
+        </div>
+
+        {/* Botón de menú para móviles */}
         <Navbar.Toggle aria-controls="navbarResponsive" />
 
-        <Navbar.Collapse id="navbarResponsive" className="justify-content-between">
-          <div className={`${styles.searchBar} input-group d-none d-lg-flex mx-auto`}>
-            <div className="input-group-prepend">
-              <span className={`${styles.searchIcon} input-group-text border-0`}>
-                <i className="bi bi-search"></i>
-              </span>
-            </div>
-            <input type="text" className="form-control border-0" placeholder="Search" />
-          </div>
-          
+        {/* Contenido colapsable (Login y Signup) */}
+        <Navbar.Collapse id="navbarResponsive" className="justify-content-end">
+          {/* Botones solo visibles en escritorio */}
           <div className="d-none d-lg-flex align-items-center">
             <button className={`btn btn-info mx-2 ${styles.buttonGroup}`}>Log In</button>
             <button className={`btn btn-info ${styles.buttonGroup}`}>Sign Up</button>
           </div>
-          
+
+          {/* Dropdown solo para móviles */}
           <div className={`${styles.mobileContainer} d-flex d-lg-none`}>
-            <div className={`${styles.searchBar} input-group mb-2`}>
-              <div className="input-group-prepend">
-                <span className={`${styles.searchIcon} input-group-text border-0`}>
-                  <i className="bi bi-search"></i>
-                </span>
-              </div>
-              <input type="text" className="form-control border-0" placeholder="Search" />
-            </div>
             <div className="d-flex justify-content-around w-100 pt-2">
               <button className={`btn btn-info ${styles.buttonGroup}`}>Log In</button>
               <button className={`btn btn-info ${styles.buttonGroup}`}>Sign Up</button>
