c789a97fb4281280042abbc73b6a7c0463e86575
Ian <CorreoIanembuste@gmail.com>
2024-11-26 17:07:53 -0400

    Se arreglo el container y ahora se muestran los datos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+// App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from '@/components/Layout/Layout';
-import HomePage from '@/pages/HomePage/HomePage';
+import HomePage from '@pages/HomePage/HomePage';
 import LoginPage from '@/pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
 
@@ -10,12 +11,12 @@ function App() {
       <Routes>
         {/* Rutas que deben estar dentro del Layout */}
         <Route element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} /> {/* HomePage se renderiza dentro del Layout */}
           <Route path="/professors" element={<h1>Future Professors Section</h1>} />
           <Route path="/events" element={<h1>Future Events Section</h1>} />
         </Route>
 
-        {/* Ruta de login fuera del Layout */}
+        {/* Rutas fuera del Layout */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,12 @@
 // src/pages/HomePage/HomePage.jsx
 import React from 'react';
-import OptionsContainer from '@/components/OptionsContainer';
+import OptionsContainer from '@/components/Contenedor/OptionsContainer';
 
-const HomePage = () => {
+function HomePage() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', padding: '1rem' }}>
+    <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main page of the application.</p>
-      <OptionsContainer />
     </div>
   );
 };
