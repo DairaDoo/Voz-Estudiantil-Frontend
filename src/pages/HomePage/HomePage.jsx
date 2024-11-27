21628454fcd20423adfae9758f126c4ca08032fe
lvctech <csjblcordero@gmail.com>
2024-11-27 19:10:23 -0400

    Se mejoro partes de la interfaz y se añadio el boton para crear nuevas reseñas. En el transcurso del progreso se mejorara el diseño y su funcionalidad.

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,16 @@
 // src/pages/HomePage/HomePage.jsx
 import React from 'react';
 import OptionsContainer from '@/components/Contenedor/OptionsContainer';
+import NewPost from '@/components/NewPost/NewPost';
 
 function HomePage() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main page of the application.</p>
+      <NewPost></NewPost>
     </div>
+    
   );
 };
 
