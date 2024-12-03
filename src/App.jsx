11b6500ea7aad5f6952283e43e8ef03cc12a6821
Ian <CorreoIanembuste@gmail.com>
2024-12-03 14:26:53 -0400

    Se arreglo el contenedor

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import HomePage from "@pages/HomePage/HomePage";
 import LoginPage from "@/pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 import ReviewsPage from "./components/ShowReviews/ShowReviews";
-import NewPost from "./components/NewPost/NewPost"; // Botón flotante
-import ReviewForm from "./components/PostReview/PostReview"; // Formulario emergente
+import NewPost from "@/components/NewPost/NewPost"; // Botón flotante
+import ReviewForm from "@/components/PostReview/PostReview"; // Formulario emergente
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
