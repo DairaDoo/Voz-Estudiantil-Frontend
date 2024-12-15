a11d68a208acf0d015c4bb29a560b0afa4713228
DairanDoo <your_email@example.com>
2024-12-14 22:14:45 -0400

    ...

--- src/components/ShowReviewCommentsModal/ShowReviewCommentsModal.jsx
+++ src/components/ShowReviewCommentsModal/ShowReviewCommentsModal.jsx
@@ -56,7 +56,7 @@ const ShowReviewCommentsModal = ({ reviewId, show, onClose }) => {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/comments", {
+      const response = await fetch("https://voz-estudiantil-backend.onrender.com/comments", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
